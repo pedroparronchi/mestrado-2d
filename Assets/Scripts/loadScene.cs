13dc530b7611125fa90e10f5cb0c89e17baa9257
Pedro Parronchi <pedrohenrique@univem.edu.br>
2020-02-19 22:30:09 -0300

    #add loading scene

---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+
+    public string scene;
+
+    public void loadSceneOnClick()
+    {
+        if(scene != null)
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+
+    }
+
+    public void teste()
+    {
+        Debug.Log("teste");
+    }
+}
